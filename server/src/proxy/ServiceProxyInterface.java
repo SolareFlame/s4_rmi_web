31f709eb71bf7494675b1b11ff24588999c22bf0
SolareFlame <zeldac332@gmail.com>
2025-06-10 14:53:11 +0200

    ✨ Add ServiceDataInterface and update ServiceProxyInterface

--- server/src/proxy/ServiceProxyInterface.java
+++ server/src/proxy/ServiceProxyInterface.java
@@ -1,4 +1,10 @@
 package proxy;
+import data.ServiceDataInterface;
+
+import java.rmi.RemoteException;
 
 public interface ServiceProxyInterface {
+    //public void enregisterDB(ServiceDatabaseInterface s_db) throws RemoteException;
+
+    public void enregisterData(ServiceDataInterface s_data) throws RemoteException;
 }
